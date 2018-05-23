938967e19c1d6262afcf08d86007005231ad45dd
leouxdesign <leoburgatte@gmail.com>
2018-05-23 17:30:44 -0400

    update

--- assets/js/active.js
+++
@@ -1,8 +0,0 @@
-    var $window = $(window);
-
-    // :: 11.0 Preloader active code
-    $window.on('load', function () {
-        $('#preloader').fadeOut('slow', function () {
-            $(this).remove();
-        });
-    });
