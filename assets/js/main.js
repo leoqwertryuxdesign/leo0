09f0b59d69c60d7e78b1727fc6df06bacc8eca90
leouxdesign <leoburgatte@gmail.com>
2018-06-19 08:36:30 -0400

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+
+
 var settings = {
 
 	banner: {
@@ -13,7 +15,7 @@ var settings = {
 			delay: 5000,
 
 		// Parallax intensity (between 0 and 1; higher = more intense, lower = less intense; 0 = off)
-			parallax: 1
+			parallax: 0.25
 
 	}
 
